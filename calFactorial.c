e517792c09b0d7e06c29cfd80e8d0b8cd77f0d7f
nilbhingradiya <70262861+nilbhingradiya@users.noreply.github.com>
2020-09-13 10:26:40 +0530

    Create calFactorial.c

---
+++ calFactorial.c
@@ -0,0 +1,14 @@
+//Write a program to calculate Factorial of a number.
+
+//This program using iteration
+
+#include<stdio.h>
+void main()
+{
+  int n,f-1,i;
+  printf("Enter a number\n");
+  scanf("%d\n",&n);
+  for(i=1;1<=n;i++)
+      f=f*i;
+  printf("Factorial is :%d",f);
+}
